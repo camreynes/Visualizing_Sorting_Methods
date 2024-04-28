6d006ccfa334a612752646eede7b8267e32967d1
CRaynes <cameronr252@gmail.com>
2024-04-27 21:02:16 -0400

    i spend probably an hour trying to write a method upCol() - it wont work. draw() messes with upCol so that if you call draw() upCol() will not work in insSort. If you dont call draw, it works. Why? I have no clue, seriously. :/

--- sort.py
+++ sort.py
@@ -1,22 +1,47 @@
 import time
 
-def bubSort(arr,draw,dur): #content,canvas,time interval
+def insSort(arr,upCol,draw,spd,can):
+    print(arr)
+    for i in range(len(arr)):
+        time.sleep(.1)
+        can = draw(arr, ['black'] * len(arr))
+        minV = arr[i]
+        mindex = i
+
+        for j in range(i, len(arr)):
+            if (arr[j] < minV):
+                mindex = j
+                minV = arr[j]
+        swap(mindex, i,arr)
+        print(arr,end=" ")
+        print(str(i) + " " + str(mindex))
+
+
+
+        time.sleep(.1)
+
+    for i in range(5):
+        upCol(i, 'green2', can)
+        time.sleep(.1)
+        can.update_idletasks()
+
+
+
+def bubSort(arr,draw,spd): #content,canvas,time interval
     n = len(arr)
     fin = False
     while not fin:
         fin = True
         for i in range(0, len(arr) - 1):
             if (arr[i + 1] < arr[i]):  # determines whether to do ascending or descending sorting
-                print("!")
                 cols = ['black'] * n #default bar color
                 cols[i],cols[i+1] = 'green2','green2' #highlights numbers beings swapped
 
+                #swaps and redraws
                 swap(i, i + 1,arr)
                 draw(arr,cols)
-
                 fin = False
-
-                time.sleep(float(dur))
+                time.sleep(float(spd))
             print("!")
 
 
--- visSort.py
+++ visSort.py
@@ -2,19 +2,19 @@ from tkinter import *
 from sort import *
 import random
 
+
 #program in tkinter to visualize bubble sort
 
 #function to draw data, colors is a list that will be used to determine each bars color
+recIDS = []  # list of rectangle ids, is appended after each rectangle is created, can.create returns rec ID
 def draw(arr,colors):
-    print(arr)
-
+    #draws inside canvas function, bars based off arr, colors tell colors of bars
     n = len(arr) #length of array, have 540x space to work with total
     horL = 540/n #length of each bar+space between next bar
     startx = 5  #starts at 5, allows 5 padding on the right
     y1 = 500  # guarentees rectangle fills beyond space
 
 
-    recIDS = []  # list of rectangle ids, is appended after each rectangle is created, can.create returns rec ID
     can.delete("all") #clears canvas
     normalize = [i/max(arr) for i in arr] #percentage of the bar that should be filled
 
@@ -23,33 +23,41 @@ def draw(arr,colors):
     for i, curr in enumerate(normalize): #curr represents the current content of normalize, being an integer
         y2 = 380-(300*curr) #height using pct rect height
         xWidth = horL * 2 / 3  # width of bar
-        xSpace = horL * 1 / 3  # space between bars
         recs = can.create_rectangle(startx,y1,startx+xWidth,380-(300*curr),fill=colors[i]) #create(x1,y1,x2,x2,col)
             #startx marks where the rectangle will start bounded, x2 is then that + the width of each bar, y is measured by content of arr
 
 
         recIDS.append(recs) #rec is an integer, then is added to the list, recIDS
-        print(22-int(n/7))
         can.create_text(startx+xWidth/2,y2-15,text=arr[i],font=("Impact",22-int(n/6)),fill="black"); #create_text(x,y,text,font=("",23),fill) - similar nature to createRect
 
         startx += horL #increment space by horL(space + width of bar)
-    root.update_idletasks() #ensures canvas is updated
+    can.update_idletasks() #ensures canvas is updated
+    return can
 
 def press():
-    print(arr)
+    #function for bubblesort
+
     spd = 1/(6*speed.get())
-    bubSort(arr,draw,spd)
+    insSort(arr,upCol,draw,spd,can)
+    #bubSort(arr, draw, spd)
 
 def rand(length):
+    #generates random arr of size length
     arr = []
     for i in range(0,length):
         arr.append(random.randint(0,10))
     return arr
 
 def upSize(size):
+    #updates size of array and redraws it
    global arr
    arr = rand(size)
    draw(arr,['black']*size)
+
+def upCol(id,col,can):
+    #updates color of bar > id
+    print("AAAAA")
+    can.itemconfig(recIDS[id], fill=col)
 
 root = Tk()
 root.title("Bubble Sort Visualization")
@@ -63,20 +71,23 @@ can = Canvas(root, width=550, height=400, bg="gray92")
 can.grid(row=0,column=0,padx=25)
 draw(arr,['black']*10)
 
-#frame for options
+# frame for options
 frame = Frame(root,width=600,height=100,bg="white")
 frame.grid(row=10,column=0)
 
 #sliders in frame
-speed = Scale(frame,from_=.5,to=5,orient=HORIZONTAL,
-             label="Speed", digits=2,resolution=.1) #size scaler
+speed = Scale(frame,from_=.5,to=5,orient=HORIZONTAL, #SPEED
+             label="Speed", digits=2,resolution=.1)
 speed.grid(row=0,column=0,pady=5)
 
-size = Scale(frame,from_=3,to=50,orient=HORIZONTAL,
+size = Scale(frame,from_=3,to=50,orient=HORIZONTAL, #SIZE
              label="Size")
 size.grid(row=0,column=1,pady=5,padx=15)
 size.bind("<ButtonRelease>", lambda value: upSize(size.get()))
-#size.config(command=lambda value: upSize(size.get()))
+
+#default values sliders
+speed.set(1)
+size.set(10)
 
 #test button
 but = Button(frame,text = "sort", height=2, width=5, fg = 'white',bg = 'black',
@@ -84,6 +95,3 @@ but = Button(frame,text = "sort", height=2, width=5, fg = 'white',bg = 'black',
 but.grid(row=10,column=0,padx=10,pady=10)
 
 root.mainloop()
-
-
-#bubSort(arr,can)
